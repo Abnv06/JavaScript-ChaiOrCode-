5e1d1d8257cb1acd41dffc1c0d08f0694b9edea3
Abnv06 <157219542+Abnv06@users.noreply.github.com>
2024-01-27 06:25:57 +0000

    learned data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+alert(3+3); //we are using node js not browser
+
+let name = 'hitesh'
+let age = 18
+let isLoggesdIn = false
+
+// number =>  range 2 to power 53
+// bigint => big number like treading websites
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbole => uniqueness
+
+console.logI(typeof name)
