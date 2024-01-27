ba87fa554a166f89125164dd4a146d3a5a5ce3de
Abnv06 <157219542+Abnv06@users.noreply.github.com>
2024-01-27 05:39:13 +0000

    learn variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "abhi001choubisa@gamil.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+let accountState;
+
+//accountCity = 2    not allowed
+
+accountEmail = "abc@gmail.com"
+accountPassword = "12345"
+accountcity ="udaipur"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
