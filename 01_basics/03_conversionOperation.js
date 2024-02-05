412638fc0c427e16632b1b470caa990ec5b2f3c2
Abnv06 <157219542+Abnv06@users.noreply.github.com>
2024-02-05 14:27:49 +0000

    operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let isLoggesdIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggesdIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// "" => false
+//"hitesh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
